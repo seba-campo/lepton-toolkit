c56a3eb851fea38a63ee8d7b55f4ccbc7951547c
Sebastián <69972223+seba-campo@users.noreply.github.com>
2023-01-10 12:53:32 -0300

    Revert "deleted"
    This reverts commit 2b11becbe1dc703b92d0eff8d42fa811376bfc8a.

---
+++ pack-template-generator/index.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+
+function parseaArgv() {
+  const _ = process.argv;
+
+  // console.log(_)
+
+  const t = parseInt(_[2]);
+  const i = parseInt(_[3]);
+  const p = _[4];
+
+  // console.log(p)
+  main(t, i, p);
+}
+
+function main(qtyTab, qtyItem, fileName) {
+  const array = [];
+
+  for (let n = 1; n <= qtyTab; n++) {
+    for (let i = 1; i <= qtyItem; i++) {
+      var result = `{pieza_obs_${n}_${i}}\t{pieza_mat_${n}_${i}}\t{pieza_c_arr_${n}_${i}}\t{pieza_c_aba_${n}_${i}}\t{pieza_c_der_${n}_${i}}\t{pieza_c_izq_${n}_${i}}\n`;
+      array.push(result);
+    }
+  }
+
+  fileName = "./" + fileName + ".txt";
+
+  fs.writeFile(fileName, array.join(""), (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("Creado el archivo----");
+      console.log(
+        "Creadas " +
+          qtyTab +
+          " tablas, con " +
+          qtyItem +
+          " valores por cada una. En el archivo: " +
+          fileName
+      );
+    }
+  });
+}
+
+parseaArgv();
---
+++ pack-template-generator/readme.txt
@@ -0,0 +1,15 @@
+/*
+    ANTES DE USAR, NECESITAS NODE, BAJAR DESDE: https://nodejs.org/dist/v16.18.0/node-v16.18.0-x64.msi
+   
+    Se debe invocar al index.js con node "node index.js"
+    el archivo espera 3 parámetros, estos tienen que estar separados por espacios.
+
+        1 parametro: cantidad de carillas
+        2 parametro: cantidad de filas por carilla
+        3 parametro: nombre del archivo a generar (string. ej: "test1") (NO USAR ESPACIOS)
+        NOTA: si el archivo ya existe, lo sobre escribe
+
+    entonces por ejemplo, "node index.js 5 45 archivoPrueba". Archivo de ejemplo en la carpeta
+
+
+*/
